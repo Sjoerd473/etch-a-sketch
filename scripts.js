c31b8d80a3924763001103072bc2f977f25c630d
Sjoerd473 <sjoerdbrussee@hotmail.com>
2024-08-20 15:29:35 +0200

    Added functionality for targeted panels changing color randomly

--- scripts.js
+++ scripts.js
@@ -3,28 +3,36 @@ function generateBackground() {
     for (let i = 0; i < 64; i++){
         let block = document.createElement('div')
         block.classList.add('panel')
-        block.addEventListener('mouseenter', function() { changeBackground('rgb(0, 0, 0)')});
-        block.addEventListener('mouseleave', function() { changeBackground('blue')});
+        block.addEventListener('mouseenter', changeBackground);
+        block.addEventListener('mouseleave', changeBackground);
+        block.addEventListener('click', changeBackground);
         container.appendChild(block);
 
     }
     
 }
 
-const randomNumber = function (){
-   let first = Math.random() * 255
-   first = Math.floor(first);
-   return first
+ function randomNumber(){
+    return Math.floor(Math.random() * 255);
+   
 }
 
-function randomColor(){
+function random(number) {
+    return Math.floor(Math.random() * (number + 1));
+  }
 
+
+
+function changeBackground(e) {
+    const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+   // const rndColor = `red`;
+    e.target.style.backgroundColor = rndCol;
+    console.log(e);
+    
 }
 
-function changeBackground(color) {
-    let one = randomNumber();
-    let two = randomNumber();
-    let three = randomNumber();
-    let panel = document.querySelector('.panel');
-    panel.style.backgroundColor = `rgb(${one},${two},${three})`
-}
+function bgChange(e) {
+    const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+    e.target.style.backgroundColor = rndCol;
+    console.log(e);
+  }
