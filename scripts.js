500bb4554aceec0837a12f6633141dc5a3944ac4
Sjoerd473 <sjoerdbrussee@hotmail.com>
2024-08-20 13:01:08 +0200

    Added function for randomizing a color

--- scripts.js
+++ scripts.js
@@ -3,7 +3,28 @@ function generateBackground() {
     for (let i = 0; i < 64; i++){
         let block = document.createElement('div')
         block.classList.add('panel')
+        block.addEventListener('mouseenter', function() { changeBackground('rgb(0, 0, 0)')});
+        block.addEventListener('mouseleave', function() { changeBackground('blue')});
         container.appendChild(block);
 
     }
+    
+}
+
+const randomNumber = function (){
+   let first = Math.random() * 255
+   first = Math.floor(first);
+   return first
+}
+
+function randomColor(){
+
+}
+
+function changeBackground(color) {
+    let one = randomNumber();
+    let two = randomNumber();
+    let three = randomNumber();
+    let panel = document.querySelector('.panel');
+    panel.style.backgroundColor = `rgb(${one},${two},${three})`
 }
