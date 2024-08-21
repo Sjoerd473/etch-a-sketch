97dd0e3739490f96be3f814a27e4eec1b75b999d
Sjoerd473 <sjoerdbrussee@hotmail.com>
2024-08-21 16:26:44 +0200

    reworked function for generating background, now generates an extra set of divs to consistently create a square

--- scripts.js
+++ scripts.js
@@ -1,38 +1,42 @@
+const startButton = document.querySelector('button');
+startButton.addEventListener('click', generateBackground);
+
+//generates the background divs
 function generateBackground() {
     const container = document.querySelector('#container');
-    for (let i = 0; i < 64; i++){
-        let block = document.createElement('div')
-        block.classList.add('panel')
-        block.addEventListener('mouseenter', changeBackground);
-        block.addEventListener('mouseleave', changeBackground);
-        block.addEventListener('click', changeBackground);
-        container.appendChild(block);
+    for (let i = 0; i < 64; i++) {
+        let column = document.createElement('div')
+        column.classList.add('column')
+        container.appendChild(column);
+
+        for (let j = 0; j < 64; j++) {
+            let block = document.createElement('div')
+            block.classList.add('block')
+            block.addEventListener('mouseenter', changeBackground);
+            block.addEventListener('mouseleave', changeBackground);
+            column.appendChild(block);
+        }
 
     }
-    
+
 }
 
- function randomNumber(){
-    return Math.floor(Math.random() * 255);
-   
-}
+
 
 function random(number) {
     return Math.floor(Math.random() * (number + 1));
-  }
-
-
-
-function changeBackground(e) {
-    const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
-   // const rndColor = `red`;
-    e.target.style.backgroundColor = rndCol;
-    console.log(e);
-    
 }
 
-function bgChange(e) {
+
+// changes the target to a random color
+function changeBackground(e) {
     const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+
     e.target.style.backgroundColor = rndCol;
-    console.log(e);
-  }
+
+
+}
+
+// write a function that calculates the width of the added divs based on the number input by the user
+
+// if input = n then width of div = co
